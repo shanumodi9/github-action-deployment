2e82f93b6aecdf2537f804bc506cdfd0d1e7aa00
shanumodi9 <70299891+shanumodi9@users.noreply.github.com>
2022-09-04 11:52:57 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,7 +1,5 @@
 <?php
 
-echo "Hello, World from Docker-PHP RAJIV demo10! <br>";
-echo "Hello AWS! <br>";
-echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
+echo "Hello, World  RAJIV demo10! <br>";
 
 ?>
